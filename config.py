1b20af3a28a54a49ddf2e3f37847d29f3b3131df
zhl2008 <zhl2008vvvss@163.com>
2016-08-18 18:08:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 sleep_time=120
-redis_ip='10.15.9.32'
+redis_ip='your redis ip'
 redis_port=6379
-redis_pass='nscnscnscnsc'
-http_ip='10.15.9.32'
+redis_pass='your redis pass'
+http_ip='your webserver ip'
 http_port='80'
-http_pass='fuckthegfw'
+http_pass='your webserver pass'
 error_log='/tmp/cloud-host/cloud_host_err.log'
 info_log='/tmp/cloud-host/cloud_host_info.log'
 resolv_list='/tmp/cloud-host/resolv.list'
